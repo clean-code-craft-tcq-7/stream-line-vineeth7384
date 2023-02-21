0de4cc0c2067621f0b5cfe83909511251cc05beb
Ranjith Prabhu Palanisamy <Palanisamy.RanjithPrabhu@in.bosch.com>
2023-02-21 09:42:37 +0530

    added receiver functions

---
+++ BMS_receive.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "BMS_receive.h"
+
+int main(void)
+{
+    char string [STRING_SIZE];
+    char substring[STRING_SIZE];
+    float voltageArray[SENSOR_VALUE_COUNT];
+    float chargeArray[SENSOR_VALUE_COUNT];
+    float minValue,maxValue,averageValue;
+    bmsMinMaxAvg bmsParameter;
+    
+    for(int i = 0; i < SENSOR_VALUE_COUNT; i++)
+    {
+        gets (string);
+        strncpy(subString, string + START_OF_VOLTAGE_VALUE, FLOAT_SIZE);
+        voltageArray[i] = strtod(subString,NULL);
+        strncpy(subString, string + START_OF_CHARGE_VALUE, FLOAT_SIZE);
+        chargeArray[i] = strtod(subString,NULL);
+    }
+
+    minMaxSensorValues(voltageArray,&minValue,&maxValue);
+    bmsParameter.voltageMin = minValue;
+    bmsParameter.voltageMin = maxValue;
+
+    minMaxSensorValues(chargeArray,&minValue,&maxValue);
+    bmsParameter.chargeMin = minValue;
+    bmsParameter.chargeMax = minValue;
+
+    movingAverage(voltageArray,&averageValue);
+    bmsParameter.voltageAverage = averageValue;
+
+    movingAverage(chargeArray,&averageValue);
+    bmsParameter.chargeAverage = averageValue;
+
+    return 0;
+}
+
+void minMaxSensorValues(float array[],float *minValue,float *maxValue)
+{
+    int i, size;
+
+    size = strlen(array);
+
+    /* Assume first element as maximum and minimum */
+    *minValue = array[0];
+    *maxValue = array[0];
+
+    for(i=1; i<size; i++)
+    {
+        /* If current element is greater than max */
+        if(array[i] > *maxValue)
+        {
+            *maxValue = array[i];
+        }
+
+        /* If current element is smaller than min */
+        if(array[i] < *minValue)
+        {
+            *minValue = array[i];
+        }
+    }
+}
+
+void movingAverage(float array[],float *avgValue)
+{
+  // the size of this array represents how many numbers will be used to calculate the average
+  float arrNumbers[5] = {0};
+  int pos = 0;
+  float newAvg = 0;
+  long sum = 0;
+  int len = 5;
+
+  for(int i = 0; i < SENSOR_VALUE_COUNT; i++)
+  {
+    sum = sum - arrNumbers[pos] + array[i];
+    arrNumbers[pos] = array[i];
+    *avgValue = sum / len ; 
+    pos++;
+    if (pos >= len)
+    {
+      pos = 0;
+    }
+  }
+}
+
