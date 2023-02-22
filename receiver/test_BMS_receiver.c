3dcd530c69e6dfecf8c85b101c1c389635e7c07f
Ranjith Prabhu Palanisamy <Palanisamy.RanjithPrabhu@in.bosch.com>
2023-02-22 10:38:21 +0530

    commit

---
+++ receiver/test_BMS_receiver.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "test_BMS_receiver.h"
+
+void readFromTest(char* testStringBuffer)
+{
+	int i = 0;
+	static float testVoltage = 1;
+    static float testCurrent = 1;
+	
+	srand(time(NULL));
+	testVoltage = testVoltage +((float) rand()/RAND_MAX);
+	testCurrent = testCurrent +((float) rand()/RAND_MAX);
+	
+	sprintf(testStringBuffer,"voltage = %.2f,current = %.2f",testVoltage,testCurrent);
+    printf("%s\n",testStringBuffer);
+}
